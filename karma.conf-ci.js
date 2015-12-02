b7caf0fbb35826ab60dfe9fda86b1181e54d406f
Mehmet Gerceker <mehmetg@saucelabs.com>
2015-12-02 11:09:38 -0800

    Stupid typo

--- karma.conf-ci.js
+++ karma.conf-ci.js
@@ -53,7 +53,7 @@ module.exports = function(config) {
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['dots', 'saucelabs'],
 
-    startConnect: false
+    startConnect: false,
     // web server port
     port: 9876,
 
